0d596b1d0889626248296c9910e133ccfd313486
Amir <47040526+AmirKhalilzadeh@users.noreply.github.com>
2023-02-18 18:42:39 +0100

    Update wc.py

---
+++ tests/wc.py
@@ -0,0 +1,29 @@
+import nbformat
+import pandas as pd
+
+
+nb_filename= 'P1.ipynb'
+nb_json = read_nb(nb_filename)
+
+def read_nb(nb_filename):
+    """Read notebooks and return content."""
+    with open(nb_filename, "rb") as nb_file:
+        txt = nb_file.read()
+    return nbformat.reads(txt, nbformat.NO_CONVERT)
+    
+# Collect markdowns 
+MD = []
+for c in nb_json['cells']:
+    if c['cell_type']=='markdown':
+        MD.append(c['source'])
+        
+# total number of words in the Markdown cells
+display('this is total number of words: ' pd.Series(MD).apply(lambda x: len(x.split())).sum())
+
+# Exclude the headers
+md=[]
+for i in range(len(MD)):
+    if MD[i].startswith('#') is False:
+        md.append(MD[i])
+        
+display('this is total number of words excluding headers: ' pd.Series(md).apply(lambda x: len(x.split())).sum())
